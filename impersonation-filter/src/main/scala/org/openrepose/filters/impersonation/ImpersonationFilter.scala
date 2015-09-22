2ea64cf359c2c3fa5037283d41f88699843caf85
dimtruck <dimitry.ushakov@rackspace.com>
2015-09-21 23:03:13 -0500

    Updated with impersonation retrieval logic

--- impersonation-filter/src/main/scala/org/openrepose/filters/impersonation/ImpersonationFilter.scala
+++ impersonation-filter/src/main/scala/org/openrepose/filters/impersonation/ImpersonationFilter.scala
@@ -21,38 +21,269 @@ package org.openrepose.filters.impersonation
 
 import javax.inject.{Inject, Named}
 import javax.servlet._
+import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
+import javax.ws.rs.core.HttpHeaders
 import org.openrepose.commons.config.manager.UpdateListener
+import org.openrepose.commons.utils.http.CommonHttpHeader
+import org.openrepose.commons.utils.servlet.http.MutableHttpServletRequest
 import org.openrepose.core.filter.FilterConfigHelper
-import org.openrepose.core.filter.logic.impl.FilterLogicHandlerDelegate
 import org.openrepose.core.services.config.ConfigurationService
 import com.typesafe.scalalogging.slf4j.LazyLogging
+import org.openrepose.core.services.datastore.{DatastoreService, Datastore}
+import org.openrepose.core.services.serviceclient.akka.{AkkaServiceClientException, AkkaServiceClient}
+import org.openrepose.core.systemmodel.SystemModel
 import org.openrepose.filters.config.RackspaceImpersonation
+import org.openrepose.filters.impersonation.ImpersonationFilter.{Reject, Pass, MissingAuthTokenException}
+import org.openrepose.filters.impersonation.ImpersonationHandler.{IdentityResponseProcessingException, OverLimitException, IdentityCommunicationException}
 
-class ImpersonationFilter @Inject()(configurationService: ConfigurationService) extends Filter with LazyLogging {
+
+import scala.concurrent.TimeoutException
+import scala.util.{Failure, Random, Success, Try}
+
+class ImpersonationFilter @Inject()(configurationService: ConfigurationService,
+                                    akkaServiceClient: AkkaServiceClient,
+                                    datastoreService: DatastoreService)
+  extends Filter with LazyLogging {
+
   private final val DEFAULT_CONFIG = "rackspace-impersonation.cfg.xml"
+  private final val SYSTEM_MODEL_CONFIG = "system-model.cfg.xml"
+  private var sendTraceHeader = true
 
-  private var config: String = _
-  private var handlerFactory: ImpersonationHandlerFactory = _
+  private val datastore: Datastore = datastoreService.getDefaultDatastore
+
+  private var impersonationConfig: RackspaceImpersonation = _
+  private var impersonationConfigFile: String = _
+
+  def isInitialized = SystemModelConfigListener.isInitialized && ImpersonationConfigListener .isInitialized
 
   override def init(filterConfig: FilterConfig): Unit = {
-    config = new FilterConfigHelper(filterConfig).getFilterConfig(DEFAULT_CONFIG)
-    logger.info(s"Initializing RackspaceImpersonationFilter using config $config")
-
-    handlerFactory = new ImpersonationHandlerFactory()
+    //load configuration file
+    impersonationConfigFile = new FilterConfigHelper(filterConfig).getFilterConfig(DEFAULT_CONFIG)
+    logger.info(s"Initializing Rackspace Impersonation Filter using config $impersonationConfigFile")
 
     val xsdURL = getClass.getResource("/META-INF/schema/config/rackspace-impersonation.xsd")
-    configurationService.subscribeTo(filterConfig.getFilterName,
-      config,
+
+
+    configurationService.subscribeTo(
+      SYSTEM_MODEL_CONFIG,
+      getClass.getResource("/META-INF/schema/system-model/system-model.xsd"),
+      SystemModelConfigListener,
+      classOf[SystemModel]
+    )
+    configurationService.subscribeTo(
+      filterConfig.getFilterName,
+      impersonationConfigFile,
       xsdURL,
-      handlerFactory.asInstanceOf[UpdateListener[RackspaceImpersonation]],
+      ImpersonationConfigListener,
       classOf[RackspaceImpersonation])
   }
 
-  override def doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain): Unit = {
-    new FilterLogicHandlerDelegate(request, response, chain).doFilter(handlerFactory.newHandler)
+  override def destroy(): Unit = {
+    configurationService.unsubscribeFrom(impersonationConfigFile, ImpersonationConfigListener)
+    configurationService.unsubscribeFrom(SYSTEM_MODEL_CONFIG, SystemModelConfigListener)
   }
 
-  override def destroy(): Unit = {
-    configurationService.unsubscribeFrom(config, handlerFactory)
+  override def doFilter(servletRequest: ServletRequest, servletResponse: ServletResponse, chain: FilterChain): Unit = {
+    /**
+     * STATIC REFERENCE TO CONFIG
+     */
+    val config = impersonationConfig
+
+    /**
+     * DECLARE COMMON VALUES
+     */
+    lazy val request = MutableHttpServletRequest.wrap(servletRequest.asInstanceOf[HttpServletRequest])
+    // Not using the mutable wrapper because it doesn't work properly at the moment, and
+    // we don't need to modify the response from further down the chain
+    lazy val response = servletResponse.asInstanceOf[HttpServletResponse]
+    lazy val traceId = Option(request.getHeader(CommonHttpHeader.TRACE_GUID.toString)).filter(_ => sendTraceHeader)
+    lazy val requestHandler = new ImpersonationHandler(
+      impersonationConfig.getAuthenticationServer.getHref, akkaServiceClient, traceId)
+
+    /**
+     * BEGIN PROCESSING
+     */
+    if (!isInitialized) {
+      logger.error("Rackspace Impersonation filter has not yet initialized")
+      response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR)
+    } else {
+      logger.debug("Rackspace Impersonation filter processing request...")
+
+      /**
+       * We have an auth token and admin token.  Let's figure out whether impersonation token exists
+       * 1. Validate impersonation token in cache
+       * 2. Doesn't exist?
+       *  2a. Get admin token
+       *  2b. Get auth token
+       */
+
+      val filterResult = {
+        val processingResult =
+          getAuthToken flatMap { authToken =>
+            getImpersonationToken(getAdminToken, authToken)
+          }
+
+        processingResult match {
+          case Success(_) => Pass
+          case Failure(e: MissingAuthTokenException) => Reject(HttpServletResponse.SC_UNAUTHORIZED, Some(e.getMessage))
+          case Failure(e: IdentityCommunicationException) => Reject(HttpServletResponse.SC_BAD_GATEWAY, Some(e.getMessage))
+          case Failure(e: OverLimitException) =>
+            response.addHeader(HttpHeaders.RETRY_AFTER, e.retryAfter)
+            Reject(HttpServletResponse.SC_SERVICE_UNAVAILABLE, Some(e.getMessage))
+          case Failure(e) if e.getCause.isInstanceOf[AkkaServiceClientException] && e.getCause.getCause.isInstanceOf[TimeoutException] =>
+            Reject(HttpServletResponse.SC_GATEWAY_TIMEOUT, Some(s"Call timed out: ${e.getMessage}"))
+          case Failure(e) => Reject(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, Some(e.getMessage))
+        }
+      }
+
+      filterResult match {
+        case Pass =>
+          logger.trace("Processing completed, passing to next filter or service")
+          chain.doFilter(request, response)
+        case Reject(statusCode, message) =>
+          Option(config.getDelegating) match {
+            case Some(delegating) =>
+              logger.debug(s"Delegating with status $statusCode caused by: ${message.getOrElse("unspecified")}")
+              logger.debug("Not delegating at the moment.  Maybe later?")
+              /*
+              val delegationHeaders = buildDelegationHeaders(statusCode,
+                "keystone-v2",
+                message.getOrElse("Failure in the Keystone v2 filter").replace("\n", " "),
+                delegating.getQuality)
+
+              delegationHeaders foreach { case (key, values) =>
+                values foreach { value =>
+                  request.addHeader(key, value)
+                }
+              }
+              chain.doFilter(request, response)
+
+              logger.trace(s"Processing response with status code: $statusCode")
+              */
+              logger.debug(s"Rejecting with status $statusCode")
+
+              message match {
+                case Some(m) =>
+                  logger.debug(s"Rejection message: $m")
+                  response.sendError(statusCode, m)
+                case None => response.sendError(statusCode)
+              }
+            case None =>
+              logger.debug(s"Rejecting with status $statusCode")
+
+              message match {
+                case Some(m) =>
+                  logger.debug(s"Rejection message: $m")
+                  response.sendError(statusCode, m)
+                case None => response.sendError(statusCode)
+              }
+          }
+      }
+    }
+
+
+    def getAuthToken: Try[String] = {
+      logger.trace("Getting the x-auth-token header value")
+
+      Option(request.getHeader(CommonHttpHeader.AUTH_TOKEN)) match {
+        case Some(token) => Success(token)
+        case None => Failure(MissingAuthTokenException("X-Auth-Token header not found"))
+      }
+    }
+
+    def getAdminToken(force: Boolean): Try[String] = {
+      logger.trace("Getting an admin token with the configured credentials")
+
+      // If force is true, clear the cache and acquire a new token
+      if (force) datastore.remove(ImpersonationHandler.ADMIN_TOKEN_KEY)
+
+      Option(datastore.get(ImpersonationHandler.ADMIN_TOKEN_KEY).asInstanceOf[String]) match {
+        case Some(cachedAdminToken) => Success(cachedAdminToken)
+        case None =>
+          requestHandler.getAdminToken(config.getAuthenticationServer.getUsername, config.getAuthenticationServer.getPassword) match {
+            case Success(adminToken) =>
+              datastore.put(ImpersonationHandler.ADMIN_TOKEN_KEY, adminToken)
+              Success(adminToken)
+            case f: Failure[_] => f
+          }
+      }
+    }
+
+
+    def getImpersonationToken(getAdminToken: Boolean => Try[String], authToken: String): Try[ImpersonationHandler.ImpersonationToken] = {
+      logger.trace(s"Retrieve impersonation token for: $authToken")
+
+      //let's see if we have an impersonation token in cache
+      Option(datastore.get(s"$ImpersonationHandler.IMPERSONATION_KEY_PREFIX$authToken").asInstanceOf[ImpersonationHandler.ImpersonationToken]) match {
+        case Some(cachedImpersonationToken) => Success(cachedImpersonationToken)
+        case None =>
+          //get some data here
+          getAdminToken(false) flatMap { validatingToken =>
+            requestHandler.getImpersonationToken(validatingToken, authToken) recoverWith {
+              case _: AdminTokenUnauthorizedException =>
+                // Force acquiring of the admin token, and call the validation function again (retry once)
+                getAdminToken(true) match {
+                  case Success(newValidatingToken) => requestHandler.validateToken(newValidatingToken, authToken)
+                  case Failure(x) => Failure(IdentityAdminTokenException("Unable to reacquire admin token", x))
+                }
+            } cacheOnSuccess { validToken =>
+              val cacheSettings = config.getCache.getTimeouts
+              val timeToLive = getTtl(cacheSettings.getToken,
+                cacheSettings.getVariability,
+                Some(validToken))
+              timeToLive foreach { ttl =>
+                datastore.put(s"$TOKEN_KEY_PREFIX$authToken", validToken, ttl, TimeUnit.SECONDS)
+              }
+            }
+          }
+
+      }
+      map
+      } { validationResult =>
+        Success(validationResult)
+      } getOrElse {
+
+      }
+    }
+
+
+  object SystemModelConfigListener extends UpdateListener[SystemModel] {
+    private var initialized = false
+
+    override def configurationUpdated(configurationObject: SystemModel): Unit = {
+      sendTraceHeader = configurationObject.isTracingHeader
+      initialized = true
+    }
+
+    override def isInitialized: Boolean = initialized
   }
+
+  object ImpersonationConfigListener extends UpdateListener[RackspaceImpersonation] {
+    private var initialized = false
+
+    override def configurationUpdated(configurationObject: RackspaceImpersonation): Unit = {
+      // Removes an extra slash at the end of the URI if applicable
+      val serviceUri = impersonationConfig.getAuthenticationServer.getHref
+      impersonationConfig.getAuthenticationServer.setHref(serviceUri.stripSuffix("/"))
+
+      initialized = true
+    }
+
+    override def isInitialized: Boolean = initialized
+  }
+
 }
+
+object ImpersonationFilter {
+
+  case class MissingAuthTokenException(message: String, cause: Throwable = null) extends Exception(message, cause)
+
+  sealed trait ImpersonationResult
+
+  object Pass extends ImpersonationResult
+
+  case class Reject(status: Int, message: Option[String] = None) extends ImpersonationResult
+
+
+}
+
--- impersonation-filter/src/main/scala/org/openrepose/filters/impersonation/ImpersonationHandler.scala
+++ impersonation-filter/src/main/scala/org/openrepose/filters/impersonation/ImpersonationHandler.scala
@@ -1,15 +1,35 @@
 package org.openrepose.filters.impersonation
 
-import javax.servlet.http.HttpServletRequest
+import java.util.{GregorianCalendar, Calendar}
+import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
+import javax.ws.rs.core.MediaType
 
+import com.fasterxml.jackson.core.JsonProcessingException
 import com.typesafe.scalalogging.slf4j.LazyLogging
+import org.apache.http.HttpHeaders
+import org.apache.http.client.utils.DateUtils
+import org.joda.time.format.ISODateTimeFormat
+import org.openrepose.commons.utils.http.{ServiceClientResponse, CommonHttpHeader}
 import org.openrepose.commons.utils.servlet.http.ReadableHttpServletResponse
 import org.openrepose.core.filter.logic.FilterDirector
 import org.openrepose.core.filter.logic.common.AbstractFilterLogicHandler
 import org.openrepose.core.filter.logic.impl.FilterDirectorImpl
 import org.openrepose.filters.config.RackspaceImpersonation
+import org.openrepose.core.services.serviceclient.akka.AkkaServiceClient
+import org.openrepose.filters.impersonation.ImpersonationHandler.{IdentityResponseProcessingException, OverLimitException, IdentityCommunicationException}
 
-class ImpersonationHandler(config: RackspaceImpersonation) extends AbstractFilterLogicHandler with LazyLogging {
+import play.api.libs.json._
+
+import scala.collection.JavaConverters._
+import scala.io.Source
+import scala.util.{Failure, Success, Try}
+
+
+class ImpersonationHandler(
+                            identityEndpoint: String,
+                            akkaServiceClient: AkkaServiceClient,
+                            traceId: Option[String])
+  extends AbstractFilterLogicHandler with LazyLogging {
 
   override def handleRequest(request: HttpServletRequest, response: ReadableHttpServletResponse): FilterDirector = {
     val filterDirector = new FilterDirectorImpl()
@@ -19,4 +39,125 @@ class ImpersonationHandler(config: RackspaceImpersonation) extends AbstractFilte
 
     filterDirector
   }
+
+  final def getAdminToken(adminUsername: String, adminPassword: String): Try[String] = {
+    //authenticate, or get the admin token
+    val authenticationPayload = Json.obj(
+      "auth" -> Json.obj(
+        "passwordCredentials" -> Json.obj(
+          "username" -> adminUsername,
+          "password" -> adminPassword
+        )
+      )
+    )
+
+    val akkaResponse = Try(akkaServiceClient.post(ImpersonationHandler.ADMIN_TOKEN_KEY,
+      s"$identityEndpoint$ImpersonationHandler.TOKEN_ENDPOINT",
+      (Map(CommonHttpHeader.ACCEPT.toString -> MediaType.APPLICATION_JSON)
+        ++ traceId.map(CommonHttpHeader.TRACE_GUID.toString -> _)).asJava,
+      Json.stringify(authenticationPayload),
+      MediaType.APPLICATION_JSON_TYPE
+    ))
+
+    akkaResponse match {
+      case Success(serviceClientResponse) =>
+        serviceClientResponse.getStatus match {
+          case statusCode if statusCode >= 200 && statusCode < 300 =>
+            val jsonResponse = Source.fromInputStream(serviceClientResponse.getData).getLines().mkString("")
+            val json = Json.parse(jsonResponse)
+            Try(Success((json \ "access" \ "token" \ "id").as[String])) match {
+              case Success(s) => s
+              case Failure(f) =>
+                Failure(IdentityCommunicationException("Token not found in identity response during admin authentication", f))
+            }
+          case HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE | ImpersonationHandler.SC_TOO_MANY_REQUESTS =>
+            Failure(OverLimitException(ImpersonationHandler.buildRetryValue(serviceClientResponse), "Rate limited when getting admin token"))
+          case statusCode if statusCode >= 500 =>
+            Failure(IdentityCommunicationException("Identity Service not available to get admin token"))
+          case _ => Failure(IdentityResponseProcessingException("Unable to successfully get admin token from Identity"))
+        }
+      case Failure(x) => Failure(IdentityResponseProcessingException("Failure communicating with identity during admin authentication", x))
+    }
+  }
+
+
+  final def getImpersonationToken(userName: String, validatingToken: String): Try[ImpersonationHandler.ImpersonationToken] = {
+    //authenticate, or get the admin token
+    val impersonationPayload = Json.obj(
+      "RAX-AUTH:impersonation" -> Json.obj(
+        "user" -> Json.obj(
+          "username" -> userName
+        ),
+        "expire-in-seconds" -> 3600
+      )
+    )
+
+    val akkaResponse = Try(akkaServiceClient.post(ImpersonationHandler.ADMIN_TOKEN_KEY,
+      s"$identityEndpoint${ImpersonationHandler.IMPERSONATION_ENDPOINT}",
+      (Map(CommonHttpHeader.AUTH_TOKEN.toString -> validatingToken,
+        CommonHttpHeader.ACCEPT.toString -> MediaType.APPLICATION_JSON)
+        ++ traceId.map(CommonHttpHeader.TRACE_GUID.toString -> _)).asJava,
+      Json.stringify(impersonationPayload),
+      MediaType.APPLICATION_JSON_TYPE
+    ))
+
+    akkaResponse match {
+      case Success(serviceClientResponse) =>
+        serviceClientResponse.getStatus match {
+          case statusCode if statusCode >= 200 && statusCode < 300 =>
+            val jsonResponse = Source.fromInputStream(serviceClientResponse.getData).getLines().mkString("")
+            try{
+              val json = Json.parse(jsonResponse)
+              //Have to convert it to a vector, because List isn't serializeable in 2.10
+              val expirationDate = ImpersonationHandler.iso8601ToRfc1123((json \ "access" \ "token" \ "expires").as[String])
+              val token = (json \ "access" \ "token" \ "id").as[String]
+              val impersonationToken = ImpersonationHandler.ImpersonationToken(expirationDate, token)
+              Success(impersonationToken)
+            } catch {
+              case oops@(_: JsResultException | _: JsonProcessingException) =>
+                Failure(IdentityCommunicationException("Unable to parse JSON from identity validate token response", oops))
+            }
+          case HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE | ImpersonationHandler.SC_TOO_MANY_REQUESTS =>
+            Failure(OverLimitException(ImpersonationHandler.buildRetryValue(serviceClientResponse), "Rate limited when getting admin token"))
+          case statusCode if statusCode >= 500 =>
+            Failure(IdentityCommunicationException("Identity Service not available to get admin token"))
+          case _ => Failure(IdentityResponseProcessingException("Unable to successfully get admin token from Identity"))
+        }
+      case Failure(x) => Failure(IdentityResponseProcessingException("Failure communicating with identity during admin authentication", x))
+    }
+  }
+}
+
+object ImpersonationHandler {
+  final val SC_TOO_MANY_REQUESTS = 429
+  final val TOKEN_ENDPOINT = "/v2.0/tokens"
+  final val IMPERSONATION_ENDPOINT = "/v2.0/RAX-AUTH/impersonation-tokens"
+  final val ADMIN_TOKEN_KEY = "IDENTITY:V2:ADMIN_TOKEN"
+  final val TOKEN_KEY_PREFIX = "IDENTITY:V2:TOKEN:"
+  final val IMPERSONATION_KEY_PREFIX = "IDENTITY:V2:IMPERSONATION_TOKEN:"
+
+  def iso8601ToRfc1123(iso: String) = {
+    val dateTime = ISODateTimeFormat.dateTimeParser().parseDateTime(iso)
+    DateUtils.formatDate(dateTime.toDate)
+  }
+
+  def buildRetryValue(response: ServiceClientResponse) = {
+    response.getHeaders.find(header => HttpHeaders.RETRY_AFTER.equalsIgnoreCase(header.getName)) match {
+      case Some(retryValue) => retryValue.getValue
+      case _ =>
+        val retryCalendar: Calendar = new GregorianCalendar
+        retryCalendar.add(Calendar.SECOND, 5)
+        DateUtils.formatDate(retryCalendar.getTime)
+    }
+  }
+
+  case class ImpersonationToken(expirationDate: String,
+                        token: String)
+
+  case class IdentityCommunicationException(message: String, cause: Throwable = null) extends Exception(message, cause) with IdentityException
+
+  case class IdentityResponseProcessingException(message: String, cause: Throwable = null) extends Exception(message, cause) with IdentityException
+
+  case class OverLimitException(retryAfter: String, message: String, cause: Throwable = null) extends Exception(message, cause) with IdentityException
+
 }
